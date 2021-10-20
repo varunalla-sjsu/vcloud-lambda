1e8898f639653a11ecf41c5f6150c838bf93a7ca
Varun Alla <varun.alla@sjsu.edu>
2021-10-20 15:57:26 -0700

    Lambda to Create File Metadata once it uploads to S3

---
+++ CreateUser/index.js
@@ -0,0 +1,42 @@
+var aws = require('aws-sdk');
+let dynamoDB=new aws.DynamoDB({apiVersion: '2012-08-10'});
+exports.handler = (event, context, callback) => {
+
+    // Send post authentication data to Cloudwatch logs
+    console.log ("Authentication successful");
+
+    console.log("complete Event",event);
+    var params = {
+        TableName: 'users',
+        Item: {
+        "username": {
+                "S": event.userName
+            },
+            "firstname": {
+                "S": event.request.userAttributes.given_name
+            },
+            "isActive": {
+                "BOOL": event.request.userAttributes['cognito:user_status']==='CONFIRMED'?true:false
+             },
+            "lastname": {
+                "S": event.request.userAttributes.family_name
+            },"email": {
+                "S": event.request.userAttributes.email
+            },
+            "Role": {
+                "N": "2"
+            }
+        }
+    };
+
+    // Call DynamoDB to add the item to the table
+    dynamoDB.putItem(params, function(err, data) {
+    if (err) {
+                console.log("Error", err);
+            } else {
+                console.log("Success", data);
+        }
+    });
+    // Return to Amazon Cognito
+    callback(null, event);
+};
